26bb54c0a7aef472f6d4e2e48d53962ee8736434
Ryan Gartner <ryanjgartner@gmail.com>
2018-12-12 18:28:14 -0600

    Psychic Game

--- assets/javascript/psychic-game.js
+++ assets/javascript/psychic-game.js
@@ -1,4 +1,4 @@
-
+// Declaring Variables
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
@@ -6,11 +6,14 @@ var losses = 0;
 var numGuesses = 9;
 var guessChoices = [];
 
+// Key press
 document.onkeyup = function(event) {
     var userGuess = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     var options = ["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
+
+// If statements
     if(options.indexOf(userGuess) > -1) {
         if(userGuess === computerGuess) {
             wins++;
