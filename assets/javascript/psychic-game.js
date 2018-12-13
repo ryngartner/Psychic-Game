753264a24fbac36c9ad34967c34c70054f890051
Ryan Gartner <ryanjgartner@gmail.com>
2018-12-12 22:58:39 -0600

    Psychic Game

--- assets/javascript/psychic-game.js
+++ assets/javascript/psychic-game.js
@@ -1,44 +1,41 @@
-// Declaring Variables
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
 var numGuesses = 9;
 var guessChoices = [];
 
-// Key press
 document.onkeyup = function(event) {
-    var userGuess = event.key;
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var playerGuess = event.key;
+    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
     var options = ["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-
-// If statements
-    if(options.indexOf(userGuess) > -1) {
-        if(userGuess === computerGuess) {
+    if(options.indexOf(playerGuess) > -1) {
+        if(playerGuess === computerGuess) {
             wins++;
             numGuesses = 9;
             guessChoices = [];
         }
-        if(userGuess != computerGuess) {
+        else if(playerGuess != computerGuess) {
             numGuesses --;
-            guessChoices.push(userGuess);
+            guessChoices.push(playerGuess);
         }
         if(numGuesses === 0 ) {
-
             numGuesses = 9;
             losses ++;
             guessChoices = [];
         }
+    
         var html = 
-        "<h1> The Pyschic Game </h1>" +
+        "<h1> The Psychic Game </h1>" +
         "<p> Guess what letter I'm thinking of</p>" +
         "<p>Wins: " + wins + "</p>" +
         "<p>Losses: " + losses + "</p>" +
-        "<p>Guesses left " + numGuesses + "</p>" +
+        "<p>Guesses left: " + numGuesses + "</p>" +
         "<p>Your Guess So far: " + guessChoices.join(", ") + "</p>";
 
-        document.querySelector("#game").innerHTML = html;
+        document.querySelector("#psychic-game").innerHTML = html;
 
     }
 };
